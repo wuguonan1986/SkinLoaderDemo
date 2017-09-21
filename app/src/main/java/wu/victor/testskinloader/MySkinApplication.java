844ea1685278361628150dfa7e1664a79e257fc5
wuguonan <wuguonan@wanda.cn>
2017-09-22 00:42:02 +0800

    first commit

---
+++ app/src/main/java/wu/victor/testskinloader/MySkinApplication.java
@@ -0,0 +1,14 @@
+package wu.victor.testskinloader;
+
+import android.app.Application;
+
+/**
+ * Created by wuguonan on 2017/2/15 0015.
+ */
+
+public class MySkinApplication extends Application {
+  @Override
+  public void onCreate() {
+    super.onCreate();
+  }
+}
